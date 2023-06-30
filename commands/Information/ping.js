7c8db23976a2e78ca7ea0b7f855a94407978c36c
Humboorgir <hardcraftap@gmail.com>
2023-06-30 22:29:10 +0330

    format ping.js in a slightly different way

--- commands/Information/ping.js
+++ commands/Information/ping.js
@@ -1,14 +1,8 @@
 const { SlashCommandBuilder } = require("discord.js");
 
-const data = new SlashCommandBuilder()
-  .setName("ping")
-  .setDescription("Replies with the bot's response time.");
-
-async function execute(message) {
-  await message.reply("Pong!");
-}
-
 module.exports = {
-  data,
-  execute,
+  data: new SlashCommandBuilder().setName("ping").setDescription("Replies with the bot's response time."),
+  async execute(message) {
+    return message.reply("Pong!");
+  },
 };
--- deploy-commands.js
+++ deploy-commands.js
@@ -26,10 +26,9 @@ for (const folder of commandFolders) {
   }
 }
 
-// Construct and prepare an instance of the REST module
 const rest = new REST().setToken(token);
 
-// and deploy your commands!
+// deploying the commands
 (async () => {
   try {
     console.log(`Started refreshing ${commands.length} application (/) commands.`);
@@ -39,7 +38,6 @@ const rest = new REST().setToken(token);
 
     console.log(`Successfully reloaded ${data.length} application (/) commands.`);
   } catch (error) {
-    // And of course, make sure you catch and log any errors!
     console.error(error);
   }
 })();
