deffe24071b00c1413dba8c57aea718c6a149788
Humboorgir <hardcraftap@gmail.com>
2023-06-30 22:10:46 +0330

    setup the command handler

--- app.js
+++ app.js
@@ -1,13 +1,61 @@
 // importing environment variables
 require("dotenv").config();
 
-const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
 
+const { Client, GatewayIntentBits, Events, Collection } = require("discord.js");
 const { Guilds } = GatewayIntentBits;
-const client = new Client({ intents: Guilds });
 
-client.once("ready", (client) => {
+const client = new Client({
+  intents: Guilds,
+});
+
+client.once(Events.ClientReady, (client) => {
   console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+
+  if (!command.data || !command.execute)
+    return console.log(`The command at ${filePath} is missing a required 'data' or 'execute' property.`);
+  client.commands.set(command.data.name, command);
+}
+
+// handling commands
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+
+    if (interaction.replied || interaction.deferred)
+      return await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
