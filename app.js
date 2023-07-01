dc372aae5d1bb0190bd789deac23bd89e340b999
Humboorgir <hardcraftap@gmail.com>
2023-07-01 21:48:49 +0330

    install yt-search

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 
-const { Client, GatewayIntentBits, Events, Collection } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const { Guilds } = GatewayIntentBits;
 
 const client = new Client({
