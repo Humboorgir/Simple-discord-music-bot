2b0ba508673efac1ed6d1a64ef0c974f06c158bc
Humboorgir <hardcraftap@gmail.com>
2023-07-03 12:19:28 +0330

    Create queue command

---
+++ commands/music/queue.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { useQueue } = require("discord-player");
+
+module.exports = {
+  data: SlashCommandBuilder.setName("queue").setDescription("Displays the current queue"),
+  execute(interaction) {
+    const queue = useQueue(interaction.guild.id);
+    const tracks = queue.tracks.toArray(); //Converts the queue into a array of tracks
+    if (!tracks.length) return interaction.reply("There is no song being played");
+    const tracksFormatted = tracks.map((track) => {
+      return `**${track.title}** from ${track.author}\n`;
+    });
+    interaction.reply(tracksFormatted);
+  },
+};
