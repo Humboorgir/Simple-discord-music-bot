624f0ff4547bcb353ef4c30d4a6283e6637856c3
Humboorgir <hardcraftap@gmail.com>
2023-07-03 23:57:24 +0330

    Create volume command

---
+++ commands/music/volume.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { useQueue } = require("discord-player");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("volume")
+    .setDescription("Changes the current playing song's volume")
+    .addIntegerOption((option) => {
+      option.setName("volume").setDescription("Select a number between 0 and 100").setRequired(true);
+    }),
+  execute(interaction) {
+    const volume = interaction.options.getInteger("volume");
+    if (!(volume >= 0 && volume <= 100))
+      return interaction.reply("Select a number between **0** and **100**");
+
+    const queue = useQueue(interaction.guild.id);
+    queue.node.setVolume(volume);
+    return interaction.reply(`Set the volume to ${volume}`);
+  },
+};
