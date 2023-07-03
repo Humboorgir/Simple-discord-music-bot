12304406d699513630096a7f697fa5783ce97bf3
Humboorgir <hardcraftap@gmail.com>
2023-07-03 16:23:48 +0330

    Fix skip commands errors

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -5,7 +5,9 @@ module.exports = {
   data: new SlashCommandBuilder().setName("skip").setDescription("Skips the current playing song"),
   execute(interaction) {
     const queue = useQueue(interaction.guild.id);
-    if (!queue.currentTrack) return interaction.reply("There is no song being played");
-    queue.node.skip();
+    if (!queue) return interaction.reply("There is no song being played");
+    interaction.reply(`Skipping **${queue.currentTrack.title}** by ${queue.currentTrack.author}`);
+
+    return queue.node.skip();
   },
 };
--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { useQueue } = require("discord-player");
+
 module.exports = {
   data: new SlashCommandBuilder().setName("stop").setDescription("Stops playing music"),
   execute(interaction) {
