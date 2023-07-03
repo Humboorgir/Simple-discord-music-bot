1d4c62e284159a1e12f1ad5ae2ccf4cc2ae5f346
Humboorgir <hardcraftap@gmail.com>
2023-07-03 12:54:32 +0330

    fix small bug

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -7,9 +7,11 @@ module.exports = {
     const queue = useQueue(interaction.guild.id);
     const tracks = queue.tracks.toArray(); //Converts the queue into a array of tracks
     if (!tracks.length) return interaction.reply("There is no song being played");
-    const tracksFormatted = tracks.map((track) => {
-      return `**${track.title}** from ${track.author}\n`;
-    });
+    const tracksFormatted = tracks
+      .map((track) => {
+        return `**${track.title}** from ${track.author}\n`;
+      })
+      .join("");
     interaction.reply(tracksFormatted);
   },
 };
