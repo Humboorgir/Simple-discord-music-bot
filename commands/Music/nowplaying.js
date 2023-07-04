ea62320551b5adb548d1a10e3c44f7c4748a7985
Humboorgir <hardcraftap@gmail.com>
2023-07-04 22:38:46 +0330

    Forgot to import EmbedBuilder in nowplaying.js

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { useQueue } = require("discord-player");
 
 module.exports = {
