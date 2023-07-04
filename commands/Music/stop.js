4b44a25cc10f5f334399edccc49a7f8f92e99ea4
Humboorgir <hardcraftap@gmail.com>
2023-07-04 22:33:11 +0330

    Fix inproper check for currentTrack being played or not

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -5,9 +5,7 @@ module.exports = {
   data: new SlashCommandBuilder().setName("stop").setDescription("Stops playing music"),
   execute(interaction) {
     const queue = useQueue(interaction.guild.id);
-    const tracks = queue.tracks.toArray();
-    if (!tracks.length)
-      return interaction.reply({ content: "There is no song being played.", ephemeral: true });
+    if (!queue) return interaction.reply({ content: "There is no song being played.", ephemeral: true });
     queue.delete();
     return interaction.reply("Stopped!");
   },
